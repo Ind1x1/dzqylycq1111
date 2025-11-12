028c30799af71720a70b986db6f44f742c16bf80
Le-yi <2777904242@qq.com>
2025-11-12 15:52:28 +0800

    add log data collector

---
+++ agent/utils/env_utils.py
@@ -0,0 +1,16 @@
+"""
+提供与运行环境相关的默认工具方法。
+"""
+
+
+def get_node_id() -> int:
+    return -1
+
+
+def get_node_type() -> str:
+    return "TRAIN_NODE"
+
+
+def get_node_rank() -> int:
+    return -1
+
